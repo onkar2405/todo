27c9c8a0f31e142b1cfd4dd5b6db89c28a0e9f8c
onkar2405 <onkarvaidyad789@gmail.com>
2024-10-15 23:02:14 +0530

    Updated methods

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/todo", function (req, res) {});
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Sent wrong inputs!!",
+    });
+    return;
+  }
+});
 app.get("/todos", function (req, res) {});
-app.put("/completed", function (req, res) {});
+
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Sent wrong inputs!!",
+    });
+    return;
+  }
+});
 
 app.listen(3000);
